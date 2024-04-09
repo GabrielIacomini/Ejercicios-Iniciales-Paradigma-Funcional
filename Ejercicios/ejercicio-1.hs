d744e5e4a3e8c20c01f5e9d7733bff86a0749f9a
GabrielIacomini <giacomini@frba.utn.edu.ar>
2024-04-09 16:46:24 -0300

    Ejercicios 2,3 y 4

--- Ejercicios/ejercicio-1.hs
+++ Ejercicios/ejercicio-1.hs
@@ -4,3 +4,21 @@
 esMultiploDeTres :: Int -> Bool
 esMultiploDeTres numero = mod numero 3 == 0
 
+-- EJERCICIO 2 -- 
+-- Definir la función esMultiploDe/2, que devuelve True si el segundo es múltiplo del primero, p.ej.  --
+
+esMultiploDe :: Int -> Int -> Bool
+esMultiploDe numero1 numero2 = mod numero1 numero2 == 0
+
+-- EJERCICIO 3 --
+-- Definir la función cubo/1, devuelve el cubo de un número --
+
+cubo :: Int -> Int
+cubo numero = numero * numero
+
+-- EJERCICIO 4 --
+-- Definir la función area/2, devuelve el área de un rectángulo a partir de su base y su altura. --
+
+area :: Num a => a -> a -> a
+area base altura = base * altura
+

4b9da21ac163bc3dd044060a679fef00d55fc8f9
GabrielIacomini <164112858+GabrielIacomini@users.noreply.github.com>
2024-04-13 20:10:31 -0300

    Update ejercicios.hs

--- Ejercicios/ejercicios.hs
+++ Ejercicios/ejercicios.hs
@@ -22,7 +22,6 @@ cubo numero = numero * numero
 area :: Num a => a -> a -> a
 area base altura = base * altura
 
-
 -- EJERCICIO 5 --
 -- Definir la función esBisiesto/1, indica si un año es bisiesto. (Un año es bisiesto si es divisible por 400 o es divisible por 4 pero no es divisible por 100) --
 
@@ -40,4 +39,40 @@ celsiusToFahr gradosEnCelcius = gradosEnCelcius * (9/5) + 32
 
 fahrToCelsius :: Floating a => a -> a
 fahrToCelsius gradosEnFahr = (gradosEnFahr - 32) * 5/9
+
+-- EJERCICIO 8 --
+-- Definir la función haceFrioF/1, indica si una temperatura expresada en grados Fahrenheit es fría. Decimos que hace frío si la temperatura es menor a 8 grados Celsius. --
+
+haceFrioF :: Float -> Bool
+haceFrioF tempEnFahr = fahrToCelsius tempEnFahr < 8
+
+-- EJERCICIO 9 --
+{- Definir la función mcm/2 que devuelva el mínimo común múltiplo entre dos números, de acuerdo a esta fórmula. m.c.m.(a, b) = {a * b} / {m.c.d.(a, b)} -} 
+
+mcm :: Integral a => a -> a -> a
+mcm entero1 entero2 = (entero1 * entero2) `div` (gcd entero1 entero2)
+
+-- EJERCICIO 10 --
+-- Definir dispersion, que toma los tres valores y devuelve la diferencia entre el más alto y el más bajo --
+-- Parte 1 --
+
+maxTres :: Ord a => a -> a -> a -> a
+maxTres dia1 dia2 dia3 = max dia1 (max dia2 dia3)
+
+minTres :: Ord a => a -> a -> a -> a
+minTres dia1 dia2 dia3 = min dia1 (min dia2 dia3)
+
+dispersion :: (Ord a, Num a) => a -> a -> a -> a
+dispersion dia1 dia2 dia3 = maxTres dia1 dia2 dia3 - minTres dia1 dia2 dia3
+
+-- Parte 2 --
+
+diasParejos :: Float -> Float -> Float -> Bool
+diasParejos dia1 dia2 dia3 = dispersion dia1 dia2 dia3 < 30
+
+diasLocos :: Float -> Float -> Float -> Bool
+diasLocos dia1 dia2 dia3 = dispersion dia1 dia2 dia3 > 100
+
+diasNormales :: Float -> Float -> Float -> Bool
+diasNormales dia1 dia2 dia3 = not (diasParejos dia1 dia2 dia3) && not (diasLocos dia1 dia2 dia3)
 
